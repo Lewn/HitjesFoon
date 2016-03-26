60311ce5410618fb1b824c86a294458e76cf78cc
Dutch-devil <lvbremen@hotmail.com>
2016-03-26 12:49:23 +0100

    List separator must be set
    Prevents memory errors

--- pc/src/widgets/WHitSearch.cpp
+++ pc/src/widgets/WHitSearch.cpp
@@ -77,6 +77,7 @@ void WHitSearch::initSearchSuggestions() {
         searchOptions.highlightEndTag = "</span>";
         searchOptions.whitespace = " \\n";
         searchOptions.wordSeparators = "-., \"@\\n;";
+        searchOptions.listSeparator = NULL;
 
         searchSP = new WSuggestionPopup(
             WSuggestionPopup::generateMatcherJS(searchOptions),
